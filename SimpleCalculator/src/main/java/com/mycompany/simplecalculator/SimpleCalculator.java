b821ae0224394ae70a734d759c17994f3f59b9e2
Rutvi Panchal <panchrut@sheridancollege.ca>
2026-06-01 19:07:38 -0400

    Add basic calculator logic

--- SimpleCalculator/src/main/java/com/mycompany/simplecalculator/SimpleCalculator.java
+++ SimpleCalculator/src/main/java/com/mycompany/simplecalculator/SimpleCalculator.java
@@ -3,14 +3,60 @@
  */
 
 package com.mycompany.simplecalculator;
+import java.util.Scanner;
 
 /**
  *
- * @author User
+ * @author Rutvi PAnchal
  */
 public class SimpleCalculator {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+ public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Simple Calculator ===");
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        System.out.println("\nChoose Operation:");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+
+        System.out.print("Enter choice: ");
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result = " + (num1 + num2));
+                break;
+
+            case 2:
+                System.out.println("Result = " + (num1 - num2));
+                break;
+
+            case 3:
+                System.out.println("Result = " + (num1 * num2));
+                break;
+
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result = " + (num1 / num2));
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid choice.");
+        }
+
+        input.close();
     }
 }
